57a040d52ff2434e4abd99de1404a731ee65ebda
Asus <avazalimov95@mail.ru>
2017-01-16 18:03:33 +0500

    Avaz Commited

---
+++ src/Activities/GuideActivity.java
@@ -0,0 +1,14 @@
+package Activities;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GuideActivity implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/Activities/Main.java
+++ src/Activities/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -17,11 +18,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Locale.setDefault(Locale.ENGLISH);
         stage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML/StartWindow.fxml"));
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("../FXML/GuideWindow.fxml"));
+        Scene scene = new Scene(root,1280,720);
         scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setMinWidth(1280);
+        primaryStage.setMinHeight(720);
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("Resources/icon.png"));
         primaryStage.show();
     }
 
