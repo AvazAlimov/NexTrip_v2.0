66170c50f75afebbc9523bd6a22f54a1bc5b0d97
Asus <avazalimov95@mail.ru>
2017-01-17 17:54:10 +0500

    Avaz Commited

--- src/Activities/Main.java
+++ src/Activities/Main.java
@@ -1,5 +1,6 @@
 package Activities;
 
+import au.com.bytecode.opencsv.CSVReader;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +10,9 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class Main extends Application {
@@ -19,7 +23,7 @@ public class Main extends Application {
         Locale.setDefault(Locale.ENGLISH);
         stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("../FXML/StartWindow.fxml"));
-        Scene scene = new Scene(root,1280,720);
+        Scene scene = new Scene(root, 1280, 720);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setMinWidth(1280);
@@ -34,11 +38,57 @@ public class Main extends Application {
         super.stop();
     }
 
-    static void closeWindow(){
+    static void closeWindow() {
         stage.hide();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @SuppressWarnings("unused")
+    private static class Language {
+        private static HashMap<String, String> currentTranslation;
+        private static HashMap<String, HashMap<String, String>> translations;
+
+        public static void init(Reader stream) throws IOException {
+            // open a CSV reader
+            CSVReader reader = new CSVReader(stream);
+
+            // list of translation HashMaps
+            translations = new HashMap<>();
+            HashMap<Integer, String> indices = new HashMap<>();
+
+            // set up columns (each language HashMap)
+            String[] next = reader.readNext();
+            if (next != null) {
+                for (int n = 1; n < next.length; n++) {
+                    translations.put(next[n], new HashMap<>());
+                    indices.put(n, next[n]);
+                }
+            }
+
+            // add definitions (cell values into proper translation HashMaps)
+            while ((next = reader.readNext()) != null) {
+                for (int n = 1; n < next.length; n++)
+                    translations.get(indices.get(n)).put(next[0], next[n]);
+            }
+
+            // close the CSV reader
+            reader.close();
+        }
+
+        public static void setLanguage(String languageName) throws NullPointerException {
+            if (!translations.containsKey(languageName))
+                throw new NullPointerException(String.format("Language %s not found!", languageName));
+
+            currentTranslation = translations.get(languageName);
+        }
+
+        public static String getTranslation(String key) throws NullPointerException {
+            if (currentTranslation == null)
+                throw new NullPointerException("Please, call setLanguage(String) before getTranslation()!");
+            return currentTranslation.get(key);
+        }
     }
 }
