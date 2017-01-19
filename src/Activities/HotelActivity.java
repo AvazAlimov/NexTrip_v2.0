fc40aed362b506b78d2bfc1c531cb4d7516a8119
LNasiba <lutfullaeva.inha@gmail.com>
2017-01-19 23:30:32 +0500

    By Nasiba

--- src/Activities/HotelActivity.java
+++ src/Activities/HotelActivity.java
@@ -1,6 +1,24 @@
 package Activities;
 
-public class HotelActivity {
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HotelActivity implements Initializable {
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void closeWindow() {
+        Main.closeWindow();
+    }
 
 
 }
