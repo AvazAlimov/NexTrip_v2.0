faaa525737c2e89d54ec653fab78eac8d555ca5f
LNasiba <lutfullaeva.inha@gmail.com>
2017-01-20 14:06:29 +0500

    By Nasiba

--- src/Activities/HotelActivity.java
+++ src/Activities/HotelActivity.java
@@ -1,8 +1,13 @@
 package Activities;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,4 +26,7 @@ public class HotelActivity implements Initializable {
     }
 
 
+    public void previousWindow() throws IOException {
+
+    }
 }
