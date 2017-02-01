fe0d433fcbe19432dcce817620f7f6ef998811ef
LNasiba <lutfullaeva.inha@gmail.com>
2017-02-01 21:52:04 +0500

    By Nasiba

---
+++ src/Activities/RestaurantActivity.java
@@ -0,0 +1,15 @@
+package Activities;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RestaurantActivity implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
