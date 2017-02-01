ecf578ad3c0d078260cd0a41efb24d3f8ca430ac
LNasiba <lutfullaeva.inha@gmail.com>
2017-02-01 21:42:04 +0500

    By Nasiba

--- src/Activities/HotelActivity.java
+++ src/Activities/HotelActivity.java
@@ -1,24 +1,39 @@
 package Activities;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.animation.SequentialTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class HotelActivity  implements Initializable {
+public class HotelActivity implements Initializable {
 
 
     public ImageView imageView;
-    private SequentialTransition root;
+    public JFXButton prevImage;
+    public JFXButton nextImage;
+    private String[] paths;
+    private int index;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        paths = new String[]{String.valueOf(getClass().getClassLoader().getResource("Resources/Pictures/bs.jpg")),
+                String.valueOf(getClass().getClassLoader().getResource("Resources/Pictures/c.jpg")),
+                String.valueOf(getClass().getClassLoader().getResource("Resources/Pictures/e.jpg")),
+                String.valueOf(getClass().getClassLoader().getResource("Resources/Pictures/m.jpg")),
+                String.valueOf(getClass().getClassLoader().getResource("Resources/Pictures/f.jpg"))};
+        index = 0;
+        imageView.setImage(new Image(paths[index]));
+
 
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -47,7 +62,23 @@ public class HotelActivity  implements Initializable {
                 }
             }
         }, 0, 5000);
-        timer.cancel();
+       // timer.cancel();
+    }
+
+    public void switchPrevImage() {
+        if (index == 0)
+            index = 5;
+        index--;
+        imageView.setImage(new Image(paths[index]));
+
+    }
+
+    public void switchNextImage() {
+        if (index == 4)
+            index = -1;
+        index++;
+        imageView.setImage(new Image(paths[index]));
+
     }
 
     public void closeWindow() {
@@ -58,6 +89,8 @@ public class HotelActivity  implements Initializable {
     public void previousWindow() throws IOException {
 
     }
+
+
 
    /* public void start() {
 
