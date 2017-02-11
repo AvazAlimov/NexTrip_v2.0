e93365da3e6a54e0bba885caa63d3d7b528c978f
Asus <avazalimov95@mail.ru>
2017-02-11 20:58:27 +0500

    Avaz Commited

--- src/Classes/Hotel.java
+++ src/Classes/Hotel.java
@@ -5,14 +5,33 @@ public class Hotel extends Guidance {
     private double endingPrice;
 
     public Hotel(String content) {
-        System.out.println(content);
+        int[] indexes = new int[11];
+        int index = 0;
+        for (int i = 0; i < content.length(); i++)
+            if (content.charAt(i) == '◎') {
+                indexes[index] = i;
+                index++;
+            }
+        setId(Integer.parseInt(content.substring(0, indexes[0])));
+        setRatings(content.substring(indexes[0] + 1, indexes[1]));
+        setName(content.substring(indexes[1] + 1, indexes[2]));
+        setInfo(content.substring(indexes[2] + 1, indexes[3]));
+        setLocation(content.substring(indexes[3] + 1, indexes[4]));
+        setImages(content.substring(indexes[4] + 1, indexes[5]));
+        setContacts(content.substring(indexes[5] + 1, indexes[6]));
+        setComments(content.substring(indexes[6] + 1, indexes[7]));
+        setAmenities(content.substring(indexes[7] + 1, indexes[8]));
+        setRatings(content.substring(indexes[8] + 1, indexes[9]));
+
+        setStartingPrice(Double.parseDouble(content.substring(indexes[9] + 1, indexes[10])));
+        setEndingPrice(Double.parseDouble(content.substring(indexes[10] + 1, indexes[11])));
     }
 
     public double getStartingPrice() {
         return startingPrice;
     }
 
-    public void setStartingPrice(double startingPrice) {
+    private void setStartingPrice(double startingPrice) {
         this.startingPrice = startingPrice;
     }
 
@@ -20,7 +39,7 @@ public class Hotel extends Guidance {
         return endingPrice;
     }
 
-    public void setEndingPrice(double endingPrice) {
+    private void setEndingPrice(double endingPrice) {
         this.endingPrice = endingPrice;
     }
 }
