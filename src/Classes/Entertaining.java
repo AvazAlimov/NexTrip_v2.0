f2c60987572e4a7c939fd41df1a4ed1b68c32b31
Asus <avazalimov95@mail.ru>
2017-02-13 12:16:07 +0500

    Avaz Commited

--- src/Classes/Entertaining.java
+++ src/Classes/Entertaining.java
@@ -10,14 +10,35 @@ public class Entertaining extends Guidance {
         rules = "";
         ageLimit = 0;
 
+        int[] indexes = new int[12];
+        int index = 0;
+        for (int i = 0; i < content.length(); i++)
+            if (content.charAt(i) == '◎') {
+                indexes[index] = i;
+                index++;
+            }
 
+        setId(Integer.parseInt(content.substring(0, indexes[0])));
+        setRating(Integer.parseInt(content.substring(indexes[0] + 1, indexes[1])));
+        setName(content.substring(indexes[1] + 1, indexes[2]));
+        setInfo(content.substring(indexes[2] + 1, indexes[3]));
+        setLocation(content.substring(indexes[3] + 1, indexes[4]));
+        setImages(content.substring(indexes[4] + 1, indexes[5]));
+        setContacts(content.substring(indexes[5] + 1, indexes[6]));
+        setComments(content.substring(indexes[6] + 1, indexes[7]));
+        setAmenities(content.substring(indexes[7] + 1, indexes[8]));
+        setRatings(content.substring(indexes[8] + 1, indexes[9]));
+
+        setPrice(Double.parseDouble(content.substring(indexes[9] + 1, indexes[10])));
+        setRules(content.substring(indexes[10] + 1, indexes[11]));
+        setAgeLimit(Integer.parseInt(content.substring(indexes[11] + 1, content.length())));
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    private void setPrice(double price) {
         this.price = price;
     }
 
@@ -25,7 +46,7 @@ public class Entertaining extends Guidance {
         return rules;
     }
 
-    public void setRules(String rules) {
+    private void setRules(String rules) {
         this.rules = rules;
     }
 
@@ -33,7 +54,7 @@ public class Entertaining extends Guidance {
         return ageLimit;
     }
 
-    public void setAgeLimit(int ageLimit) {
+    private void setAgeLimit(int ageLimit) {
         this.ageLimit = ageLimit;
     }
 }
