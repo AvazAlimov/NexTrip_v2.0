8a8e688ad6438c6851078467196bdaf8c33c165f
Asus <avazalimov95@mail.ru>
2017-02-16 11:47:22 +0500

    Avaz Commited

--- src/Activities/Main.java
+++ src/Activities/Main.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -18,6 +19,7 @@ import javafx.stage.StageStyle;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
@@ -271,4 +273,76 @@ public class Main extends Application {
         stream.close();
         serverSocket.close();
     }
+
+    //region Tools
+    static String filledStar = "\"M121.215,44.212l-34.899-3.3c-2.2-0.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101,0l-12.4,30.3 c-0.8,2.1-2.8,3.5-5,3.7l-34.9,3.3c-5.2,0.5-7.3,7-3.4,10.5l26.3,23.1c1.7,1.5,2.4,3.7,1.9,5.9l-7.9,32.399 c-1.2,5.101,4.3,9.3,8.9,6.601l29.1-17.101c1.9-1.1,4.2-1.1,6.1,0l29.101,17.101c4.6,2.699,10.1-1.4,8.899-6.601l-7.8-32.399 c-0.5-2.2,0.2-4.4,1.9-5.9l26.3-23.1C128.615,51.212,126.415,44.712,121.215,44.212z\"";
+    static String emptyStar = "\"M453.443 160.214l-119.7-8.6c-2.5-0.2-4.7-1.7-5.6-4.1l-45.6-111c-6.3-15.3-20.5-24.8-37.1-24.8s-30.7 9.5-37.1 24.9   l-45.7 111.1c-1 2.3-3.1 3.9-5.6 4.1l-119.5 8.6c-16.2 0.9-30 11.5-35.3 27.1c-5.4 15.8-0.9 32.4 11.9 43.3l92.5 76.4   c1.9 1.6 2.8 4.2 2.2 6.6l-28.2 115.4c-3.3 12-1 24.4 6.3 34.1c7.5 9.8 19.2 15.7 31.5 15.7c7.6 0 15-2.3 21.1-6.4l101.1-62.9   c2.1-1.3 4.8-1.3 6.9 0l102.1 62.7c6.4 4.3 13.8 6.6 21.3 6.6c11.5 0 22.9-5.5 30.3-14.8c7.7-9.6 10.5-22.1 7.7-34.6l-28.3-115.7   c-0.6-2.5 0.3-5.1 2.2-6.7l93.9-76.6c12.6-10.8 17.1-27.4 11.8-43.2C483.343 171.814 469.443 161.114 453.443 160.214z    M461.143 211.714l-93.7 76.5c-9.2 7.5-13.4 19.9-10.5 31.5l28.3 115.5c1.5 6.8-1.2 11.5-3 13.7c-2.8 3.5-7 5.7-11.2 5.7   c-2.6 0-5.2-0.8-8-2.7l-102.5-63c-4.9-3-10.5-4.6-16.3-4.6s-11.5 1.6-16.4 4.7l-101.5 63.1c-6.5 4.3-15.2 2.4-19.8-3.6   c-1.9-2.5-3.9-6.8-2.1-13.1l28.3-115.8c2.8-11.5-1.3-23.8-10.4-31.3l-92.3-76.3c-7.3-6.3-5.3-14.4-4.5-16.7s4.1-10 13.7-10.5   l119.7-8.6c11.8-0.9 22-8.2 26.5-19.2l45.7-111.1c3.7-9 12-9.7 14.4-9.7c2.4 0 10.7 0.7 14.4 9.7l45.7 111.1   c4.5 11 14.7 18.3 26.5 19.2l119.9 8.7c9.4 0.5 12.7 8.2 13.5 10.5C466.143 197.514 468.143 205.614 461.143 211.714z\"";
+    static Hotel hotel;
+    static Restaurant restaurant;
+    static ThingsToDo thingsToDo;
+    static Entertaining entertaining;
+
+    static boolean contains(String word, String part) {
+        if (part.length() > word.length())
+            return false;
+        for (int i = 0; i < word.length(); i++) {
+            if ((i + part.length() - 1) < word.length()) {
+                boolean isPart = true;
+                for (int j = 0; j < part.length(); j++)
+                    if (word.charAt(i + j) != part.charAt(j))
+                        isPart = false;
+                if (isPart)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    static ArrayList<Hotel> findHotel(String location) {
+        ArrayList<Hotel> returnHotels = new ArrayList<>();
+        for (Hotel hotel : hotels)
+            if (contains(hotel.getLocation().toLowerCase(), location.toLowerCase()))
+                returnHotels.add(hotel);
+        return returnHotels;
+    }
+
+    static ArrayList<Restaurant> findRestaurants(String location) {
+        ArrayList<Restaurant> returnRestaurants = new ArrayList<>();
+        for (Restaurant restaurant : restaurants)
+            if (contains(restaurant.getLocation().toLowerCase(), location.toLowerCase()))
+                returnRestaurants.add(restaurant);
+        return returnRestaurants;
+    }
+
+    static ArrayList<Entertaining> findEntertainings(String location){
+        ArrayList<Entertaining> returnEntertainings = new ArrayList<>();
+        for (Entertaining entertaining : entertainings)
+            if (contains(entertaining.getLocation().toLowerCase(), location.toLowerCase()))
+                returnEntertainings.add(entertaining);
+        return returnEntertainings;
+    }
+
+    static ArrayList<ThingsToDo> findThingsToDo(String location, DatePicker startDate, DatePicker endDate) {
+        ArrayList<ThingsToDo> returnThingsToDo = new ArrayList<>();
+        for (ThingsToDo thingsToDo : thingsToDos) {
+            DatePicker start = new DatePicker();
+            start.setValue(LocalDate.of(thingsToDo.getStartDate().getYear(), thingsToDo.getStartDate().getMonth(), thingsToDo.getStartDate().getDay()));
+            DatePicker end = new DatePicker();
+            end.setValue(LocalDate.of(thingsToDo.getEndDate().getYear(), thingsToDo.getEndDate().getMonth(), thingsToDo.getEndDate().getDay()));
+
+            if (contains(thingsToDo.getLocation().toLowerCase(), location.toLowerCase()) && start.getValue().isBefore(startDate.getValue()) || start.getValue().isEqual(startDate.getValue()) && (end.getValue().isAfter(endDate.getValue()) || end.getValue().isEqual(endDate.getValue())))
+                returnThingsToDo.add(thingsToDo);
+        }
+        return returnThingsToDo;
+    }
+
+    static boolean checkDate(DatePicker startDate, DatePicker endDate, ThingsToDo thingsToDo) {
+        DatePicker start = new DatePicker();
+        start.setValue(LocalDate.of(thingsToDo.getStartDate().getYear(), thingsToDo.getStartDate().getMonth(), thingsToDo.getStartDate().getDay()));
+        DatePicker end = new DatePicker();
+        end.setValue(LocalDate.of(thingsToDo.getEndDate().getYear(), thingsToDo.getEndDate().getMonth(), thingsToDo.getEndDate().getDay()));
+
+        return (start.getValue().isBefore(startDate.getValue()) || start.getValue().isEqual(startDate.getValue())) && (end.getValue().isAfter(endDate.getValue()) || end.getValue().isEqual(endDate.getValue()));
+    }
+    //endregion
 }
---
+++ src/Activities/MainActivity.java
@@ -0,0 +1,462 @@
+package Activities;
+
+import Classes.Entertaining;
+import Classes.Hotel;
+import Classes.Restaurant;
+import Classes.ThingsToDo;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+@SuppressWarnings("Duplicates")
+public class MainActivity implements Initializable {
+    public VBox container;
+    public HBox datePane;
+    public ComboBox<String> searchText;
+    public DatePicker startDate;
+    public DatePicker endDate;
+    private String choosenType = "Hotels";
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void switchTabs(ActionEvent event) {
+        Button btn = (Button) event.getSource();
+        HBox box = (HBox) btn.getParent();
+        for (Node item : box.getChildren())
+            item.setStyle("-fx-background-color: #FFC107;");
+        choosenType = btn.getText();
+        container.getChildren().clear();
+        switch (btn.getText()) {
+            case "Hotels":
+                datePane.setVisible(false);
+                break;
+            case "Restaurants":
+                datePane.setVisible(false);
+                break;
+            case "Entertaining":
+                datePane.setVisible(false);
+                break;
+            case "Things To Do":
+                datePane.setVisible(true);
+                break;
+            case "Log In":
+                try {
+                    Parent parent = FXMLLoader.load(getClass().getResource("../FXML/SignInWindow.fxml"));
+                    Scene scene = new Scene(parent);
+                    Main.stage.hide();
+                    Main.stage.setScene(scene);
+                    Main.stage.show();
+                } catch (IOException e) {
+                    System.out.println("FXML file was not founded");
+                }
+                break;
+        }
+        btn.setStyle("-fx-background-color: #FFA000;");
+    }
+
+    public void searchObject() {
+        String text = searchText.getEditor().getText();
+        if (text.isEmpty())
+            return;
+
+        searchText.getItems().clear();
+
+        switch (choosenType) {
+            case "Hotels":
+                for (Hotel hotel : Main.hotels) {
+                    if (Main.contains(hotel.getLocation().toLowerCase(), text.toLowerCase())) {
+                        searchText.getItems().add(hotel.getLocation());
+                    }
+                }
+                break;
+            case "Entertaining":
+                for (Entertaining entertaining : Main.entertainings)
+                    if (Main.contains(entertaining.getLocation().toLowerCase(), text.toLowerCase())) {
+                        searchText.getItems().add(entertaining.getLocation());
+                    }
+                break;
+            case "Restaurants":
+                for (Restaurant restaurant : Main.restaurants) {
+                    if (Main.contains(restaurant.getLocation().toLowerCase(), text.toLowerCase())) {
+                        searchText.getItems().add(restaurant.getLocation());
+                    }
+                }
+                break;
+            case "Things To Do":
+                try {
+                    for (ThingsToDo thingsToDo : Main.thingsToDos)
+                        if (Main.contains(thingsToDo.getLocation().toLowerCase(), text.toLowerCase()) && Main.checkDate(startDate, endDate, thingsToDo))
+                            searchText.getItems().add(thingsToDo.getLocation());
+
+                } catch (Exception ignored) {
+                }
+
+                break;
+            default:
+                break;
+        }
+        searchText.show();
+    }
+
+    public void addObjects() {
+        switch (choosenType) {
+            case "Hotels":
+                addHotels();
+                break;
+            case "Restaurants":
+                addRestaurants();
+                break;
+            case "Entertaining":
+                addEntertaining();
+                break;
+            case "Things To Do":
+                try {
+                    addThingsToDo();
+                } catch (Exception ignored) {
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void addHotels() {
+        ArrayList<Hotel> hotels = Main.findHotel(searchText.getEditor().getText());
+        container.getChildren().clear();
+        for (Hotel hotel : hotels)
+            container.getChildren().add(fillHotelItem(hotel));
+    }
+
+    private void addRestaurants() {
+        ArrayList<Restaurant> restaurants = Main.findRestaurants(searchText.getEditor().getText());
+        container.getChildren().clear();
+        for (Restaurant restaurant : restaurants)
+            container.getChildren().add(fillRestaurantItem(restaurant));
+    }
+
+    private void addThingsToDo() {
+        ArrayList<ThingsToDo> thingsToDos = Main.findThingsToDo(searchText.getEditor().getText(), startDate, endDate);
+        container.getChildren().clear();
+        for (ThingsToDo thingsToDo : thingsToDos)
+            container.getChildren().add(fillThingsToDoItem(thingsToDo));
+    }
+
+    private void addEntertaining() {
+        ArrayList<Entertaining> entertainings = Main.findEntertainings(searchText.getEditor().getText());
+        container.getChildren().clear();
+        for (Entertaining entertaining : entertainings)
+            container.getChildren().add(fillEntertainingItem(entertaining));
+    }
+
+    private GridPane fillHotelItem(Hotel hotel) {
+        GridPane item = new GridPane();
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setHgrow(Priority.SOMETIMES);
+        ColumnConstraints col3 = new ColumnConstraints();
+        item.getColumnConstraints().addAll(col1, col2, col3);
+        item.setHgap(10);
+        item.setStyle("-fx-padding: 10; -fx-background-color: rgba(0, 100, 100, 0.5);");
+
+        URL url = null;
+        try {
+            url = new File(hotel.getPhotos().get(0)).toURI().toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Image value = null;
+        if (url != null)
+            value = new Image(url.toString(), 100.0, 100.0, false, true);
+
+        ImageView image = new ImageView(value);
+        Circle circle = new Circle(50.0);
+        circle.setCenterX(50.0);
+        circle.setCenterY(50.0);
+        image.setClip(circle);
+        item.add(image, 0, 0);
+
+        Label info = new Label("Name: " + hotel.getName() + "\nPrice: " + hotel.getStartingPrice() + "$ - " + hotel.getEndingPrice() + "$\nLocation: " + hotel.getLocation());
+        info.setStyle("-fx-font-size: 24; -fx-alignment: center-left;");
+        item.add(info, 1, 0);
+
+        HBox ratingBox = new HBox(5.0);
+        ratingBox.setStyle("-fx-alignment: center;");
+
+        int maxRate = 5;
+        for (int i = 0; i < hotel.getRating(); i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.filledStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+            maxRate--;
+        }
+        for (int i = 0; i < maxRate; i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.emptyStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+        }
+        item.add(ratingBox, 2, 0);
+
+        item.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    Main.hotel = hotel;
+                    Parent parent = FXMLLoader.load(getClass().getResource("../FXML/HotelWindow.fxml"));
+                    Scene scene = new Scene(parent);
+                    Main.stage.hide();
+                    Main.stage.setScene(scene);
+                    Main.stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return item;
+    }
+
+    private GridPane fillRestaurantItem(Restaurant restaurant) {
+        GridPane item = new GridPane();
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setHgrow(Priority.SOMETIMES);
+        ColumnConstraints col3 = new ColumnConstraints();
+        item.getColumnConstraints().addAll(col1, col2, col3);
+        item.setHgap(10);
+        item.setStyle("-fx-padding: 10; -fx-background-color: rgba(0, 100, 100, 0.5);");
+
+        URL url = null;
+        try {
+            url = new File(restaurant.getPhotos().get(0)).toURI().toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Image value = null;
+        if (url != null)
+            value = new Image(url.toString(), 100.0, 100.0, false, true);
+
+        ImageView image = new ImageView(value);
+        Circle circle = new Circle(50.0);
+        circle.setCenterX(50.0);
+        circle.setCenterY(50.0);
+        image.setClip(circle);
+        item.add(image, 0, 0);
+
+        Label info = new Label("Name: " + restaurant.getName() + "\nType: " + restaurant.getType().get(0) + "\t Number of seats: " + restaurant.getNumberOfSeats() + "\nLocation: " + restaurant.getLocation());
+        info.setStyle("-fx-font-size: 24; -fx-alignment: center-left;");
+        item.add(info, 1, 0);
+
+        HBox ratingBox = new HBox(5.0);
+        ratingBox.setStyle("-fx-alignment: center;");
+
+        int maxRate = 5;
+        for (int i = 0; i < restaurant.getRating(); i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.filledStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+            maxRate--;
+        }
+        for (int i = 0; i < maxRate; i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.emptyStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+        }
+        item.add(ratingBox, 2, 0);
+
+        item.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    Main.restaurant = restaurant;
+                    Parent parent = FXMLLoader.load(getClass().getResource("../FXML/RestaurantWindow.fxml"));
+                    Scene scene = new Scene(parent);
+                    Main.stage.hide();
+                    Main.stage.setScene(scene);
+                    Main.stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return item;
+    }
+
+    private GridPane fillThingsToDoItem(ThingsToDo thingsToDo) {
+        GridPane item = new GridPane();
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setHgrow(Priority.SOMETIMES);
+        ColumnConstraints col3 = new ColumnConstraints();
+        item.getColumnConstraints().addAll(col1, col2, col3);
+        item.setHgap(10);
+        item.setStyle("-fx-padding: 10; -fx-background-color: rgba(0, 100, 100, 0.5);");
+
+        URL url = null;
+        try {
+            url = new File(thingsToDo.getPhotos().get(0)).toURI().toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Image value = null;
+        if (url != null)
+            value = new Image(url.toString(), 100.0, 100.0, false, true);
+
+        ImageView image = new ImageView(value);
+        Circle circle = new Circle(50.0);
+        circle.setCenterX(50.0);
+        circle.setCenterY(50.0);
+        image.setClip(circle);
+        item.add(image, 0, 0);
+
+        Label info = new Label("Name: " + thingsToDo.getName() + "\nPrice: " + thingsToDo.getPrice() + " $\t" + "\nLocation: " + thingsToDo.getLocation());
+        info.setStyle("-fx-font-size: 24; -fx-alignment: center-left;");
+        item.add(info, 1, 0);
+
+        HBox ratingBox = new HBox(5.0);
+        ratingBox.setStyle("-fx-alignment: center;");
+
+        int maxRate = 5;
+        for (int i = 0; i < thingsToDo.getRating(); i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.filledStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+            maxRate--;
+        }
+        for (int i = 0; i < maxRate; i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.emptyStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+        }
+        item.add(ratingBox, 2, 0);
+
+        item.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    Main.thingsToDo = thingsToDo;
+                    Parent parent = FXMLLoader.load(getClass().getResource("../FXML/ThingsToDoWindow.fxml"));
+                    Scene scene = new Scene(parent);
+                    Main.stage.hide();
+                    Main.stage.setScene(scene);
+                    Main.stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return item;
+    }
+
+    private GridPane fillEntertainingItem(Entertaining entertaining) {
+        GridPane item = new GridPane();
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setHgrow(Priority.SOMETIMES);
+        ColumnConstraints col3 = new ColumnConstraints();
+        item.getColumnConstraints().addAll(col1, col2, col3);
+        item.setHgap(10);
+        item.setStyle("-fx-padding: 10; -fx-background-color: rgba(0, 100, 100, 0.5);");
+
+        URL url = null;
+        try {
+            url = new File(entertaining.getPhotos().get(0)).toURI().toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Image value = null;
+        if (url != null)
+            value = new Image(url.toString(), 100.0, 100.0, false, true);
+
+        ImageView image = new ImageView(value);
+        Circle circle = new Circle(50.0);
+        circle.setCenterX(50.0);
+        circle.setCenterY(50.0);
+        image.setClip(circle);
+        item.add(image, 0, 0);
+
+        Label info = new Label("Name: " + entertaining.getName() + "\nPrice: " + entertaining.getPrice() + " $\t" + "\nLocation: " + entertaining.getLocation());
+        info.setStyle("-fx-font-size: 24; -fx-alignment: center-left;");
+        item.add(info, 1, 0);
+
+        HBox ratingBox = new HBox(5.0);
+        ratingBox.setStyle("-fx-alignment: center;");
+
+        int maxRate = 5;
+        for (int i = 0; i < entertaining.getRating(); i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.filledStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+            maxRate--;
+        }
+        for (int i = 0; i < maxRate; i++) {
+            Button star = new Button();
+            star.setPrefSize(40.0, 40.0);
+            star.setStyle("-fx-shape: " + Main.emptyStar);
+            star.setDisable(true);
+            ratingBox.getChildren().add(star);
+        }
+        item.add(ratingBox, 2, 0);
+
+        item.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    Main.entertaining = entertaining;
+                    Parent parent = FXMLLoader.load(getClass().getResource("../FXML/EntertainingWindow.fxml"));
+                    Scene scene = new Scene(parent);
+                    Main.stage.hide();
+                    Main.stage.setScene(scene);
+                    Main.stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return item;
+    }
+}
+
