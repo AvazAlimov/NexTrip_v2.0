505f9adeea61dc1c94c986c1ee222820a5cbb359
Asus <avazalimov95@mail.ru>
2017-02-18 14:46:02 +0500

    Avaz Commited

--- src/Activities/MainActivity.java
+++ src/Activities/MainActivity.java
@@ -105,7 +105,6 @@ public class MainActivity implements Initializable {
     }
 
     public void addObjects() {
-
         switch (choosenType) {
             case "Hotels":
                 addHotels();
